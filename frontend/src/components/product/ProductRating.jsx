7a0902430e1332ab29fedfa02b1a1ae0a03d2ebe
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 14:10:18 +0800

    Added ProductRatings component

--- frontend/src/components/product/ProductRating.jsx
+++ frontend/src/components/product/ProductRating.jsx
@@ -1,5 +1,38 @@
 const ProductRating = () => {
-    return ( <></> );
+  const rating = 3.7
+  const max_stars = 5
+  const filled_stars = Math.floor(rating)
+
+  const renderStars = () => {
+    const stars = []
+
+    for (let i = 0; i < filled_stars; i++) {
+      stars.push(
+        <span key={i} className="text-yellow-400 text-2xl">
+          &#9733;
+        </span>
+      );
+    }
+
+    const remaining_stars = max_stars - filled_stars
+
+    for (let i = 0; i < remaining_stars; i++) {
+      stars.push(
+        <span key={filled_stars + i} className="text-gray-300 text-2xl">
+          &#9734;
+        </span>
+      )
+    }
+
+    return stars
+  };
+
+  return (
+    <div className="rating-stars flex items-center">
+      {renderStars()}
+      <span className="rating-value ml-2">{rating}</span>
+    </div>
+  )
 }
- 
-export default ProductRating;
+
+export default ProductRating
