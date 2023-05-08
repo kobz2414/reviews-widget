30b4df6c6159a36d1dac746dedda7d0d837fda53
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 10:49:27 +0800

    Added connection to database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const server = express()
 const dotenv = require("dotenv")
+const db = require("./models")
 
-//Set up global configuration access
+//Allow environment variables access
 dotenv.config()
 
-const PORT = process.env.PORT || 4000
+//Connect to database
+db.mongoose
+    .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("Successfully connected to database.")
+    }).catch(err => {
+        console.error("Connection error: ", err.message)
+        process.exit()
+    })
+
+const PORT = process.env.PORT || 4001
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
