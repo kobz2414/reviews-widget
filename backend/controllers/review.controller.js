da75a7d6172e98d9f3b52b5b46437baa20f47f6f
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 11:14:13 +0800

    Added upvoteReivew function

--- backend/controllers/review.controller.js
+++ backend/controllers/review.controller.js
@@ -1,2 +1,16 @@
 const db = require('../models')
 const Review = db.review
+
+exports.upvoteReview = async () => {
+    try{
+        //Get new data from request object
+        const { reviewId } = req.params
+
+        //Find and increment review upvote count
+        await Review.updateOne({ _id: reviewId }, { $inc: { upvotes: 1 } })
+
+        return res.status(200).json({ error: "Successfully upvoted" })
+    }catch (err){
+        return res.status(400).json({error: "Something went wrong", message: err.message})
+    }
+}
