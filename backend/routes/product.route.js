aaf1b4c12571aa657996254f34a1991d777af448
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 11:10:28 +0800

    Added getProuctRatings route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,4 +3,5 @@ const controller = require('../controllers/product.controller')
 module.exports = (server) => {
     server.post("/api/products/:productId/reviews", controller.addReview)
     server.get("/api/products/:productId/reviews", controller.getProductReviews)
+    server.get("/api/products/:productId/rating", controller.getProductRatings)
 }
