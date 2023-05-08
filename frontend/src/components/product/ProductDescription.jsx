4eb58d9a17795d2a30b9ebf264023ca6fb5ba7bf
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 13:26:09 +0800

    Added product description component

---
+++ frontend/src/components/product/ProductDescription.jsx
@@ -0,0 +1,19 @@
+const ProductDescription = () => {
+  return (
+    <>
+      <p className=" my-4">
+        The MacBook Air 2022 is a lightweight and powerful laptop from Apple. It
+        features Apple&apos;s latest M2 chip, which offers improved performance
+        and energy efficiency compared to previous models. The laptop also
+        boasts a high-resolution Retina display and a sleek, ultra-thin design.
+        With up to 16GB of RAM and up to 2TB of storage, the MacBook Air 2022 is
+        capable of handling even the most demanding tasks, from intensive
+        workloads to gaming and multimedia editing. Additionally, the laptop
+        comes equipped with macOS Monterey, the latest operating system from
+        Apple, which offers new features and improved functionality.
+      </p>
+    </>
+  );
+};
+
+export default ProductDescription;
