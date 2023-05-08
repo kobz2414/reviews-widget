d1e6d077e80828965c6edcfe87e3ade5aa081bb5
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 13:54:33 +0800

    Updated product specification content

--- frontend/src/components/product/ProductSpecification.jsx
+++ frontend/src/components/product/ProductSpecification.jsx
@@ -1,14 +1,24 @@
 const ProductSpecification = () => {
     const product_specifications = [
+        { title: "Brand", value: "Apple" },
+        { title: "Model Name", value: "Macbook Air" },
+        { title: "Screen Size", value: "13.6 Inches" },
+        { title: "Color", value: "Space Grey" },
+        { title: "Hard Disk Size", value: "256 GB" },
+        { title: "CPU Model", value: "M2" },
+        { title: "RAM", value: "8 GB" },
+        { title: "Operating System", value: "Mac OS" },
+        { title: "Graphics Card", value: "Integrated" },
+        { title: "Other", value: "Backlit Keyboard" },
       ];
 
     return ( <>
         <div className="flex justify-center items-center m-auto p-8 outline-dashed">
-          <div className="grid grid-cols-2 gap-4 w-full outline-dashed">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full outline-dashed">
             {product_specifications.map((spec, index) => (
-              <div key={index} className="p-2">
-                <span className="text-base font-bold mb-2">{spec.title} </span>
-                <span className="">{spec.value}</span>
+              <div key={index}>
+                <span className="text-base font-bold">{spec.title} </span>
+                <span>{spec.value}</span>
               </div>
             ))}
           </div>
