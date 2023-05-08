4b2b87cca9cde9fa97267877ad8b146bdf336e25
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-09 01:07:59 +0800

    Added comments for route information

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,7 +1,10 @@
 const controller = require('../controllers/product.controller')
 
 module.exports = (server) => {
+    //Add reviews under a productID
     server.post("/api/products/:productId/reviews", controller.addReview)
+    //Get all reviews under a productID
     server.get("/api/products/:productId/reviews", controller.getProductReviews)
+    //Get the average product rating from reviews
     server.get("/api/products/:productId/rating", controller.getProductRatings)
 }
--- backend/routes/review.route.js
+++ backend/routes/review.route.js
@@ -1,6 +1,8 @@
 const controller = require('../controllers/review.controller')
 
 module.exports = (server) => {
+    //Upvote a review
     server.patch("/api/reviews/:reviewId/upvote", controller.upvoteReview)
+    //Downvote a review
     server.patch("/api/reviews/:reviewId/downvote", controller.downvoteReview)
 }
