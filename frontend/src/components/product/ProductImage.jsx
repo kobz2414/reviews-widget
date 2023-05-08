11ce009a2adb4bc2361378f3df4950b3427e7135
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 13:26:00 +0800

    Added product image component

---
+++ frontend/src/components/product/ProductImage.jsx
@@ -0,0 +1,13 @@
+const ProductImage = () => {
+  return (
+    <>
+      <img
+        src="MacbookAir2023.jpg"
+        alt="Macbook Pro 2022"
+        className="object-cover h-auto sm:h-auto w-full rounded-2xl"
+      />
+    </>
+  );
+};
+
+export default ProductImage;
