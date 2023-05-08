d735590671e1e7084f1b0b0ad6fb9abbb4faf606
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 18:44:21 +0800

    Updated navbar contents

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,92 +1,20 @@
-import { useState } from "react";
-
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav className="bg-zinc-900">
       <div className="max-w-8xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-center">
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Specifications
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Reviews
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="block sm:hidden">
-            <button
-              onClick={toggleNavbar}
-              className="text-gray-300 hover:text-white focus:outline-none focus:text-white"
-            >
-              <svg
-                className="h-6 w-6 fill-current"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+            <div className="flex space-x-4">
+              <p
+                href="#"
+                className="text-gray-300 px-3 py-2 rounded-md text-sm font-bold"
               >
-                {isOpen ? (
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M5 8h14v2H5zm0 5h14v2H5zm0 5h14v2H5z"
-                  />
-                ) : (
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"
-                  />
-                )}
-              </svg>
-            </button>
+                eStore
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      {isOpen && (
-        <div className="sm:hidden">
-          <div className="flex items-center flex-col px-2 pt-2 pb-3 space-y-1">
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Specifications
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Reviews
-            </a>
-          </div>
-        </div>
-      )}
     </nav>
   );
 };
