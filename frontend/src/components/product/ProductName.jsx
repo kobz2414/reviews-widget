b5413b4d28825c4112b64eab3cf77c48539c0834
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 13:25:54 +0800

    Added product name component

---
+++ frontend/src/components/product/ProductName.jsx
@@ -0,0 +1,10 @@
+const ProductName = () => {
+  return (
+    <>
+      <h2 className="text-5xl font-bold mb-2">Macbook Air 2023</h2>
+      <p className="text-sm text-gray-600">Brand: Apple</p>
+    </>
+  );
+};
+
+export default ProductName;
