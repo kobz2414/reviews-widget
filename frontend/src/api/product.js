360ebd2d9bb6a44727289d9f6075b3ec4c8e71eb
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 17:10:13 +0800

    Added upvote and downvote review fns

--- frontend/src/api/product.js
+++ frontend/src/api/product.js
@@ -1,9 +1,27 @@
 import axios from "axios"
 const API_URL = "http://localhost:4000"
 
-export async function getReviews() {
+export async function getReviews(productId) {
     try{
-        return await axios.get(`${API_URL}/api/products/123123/reviews`)
+        return await axios.get(`${API_URL}/api/products/${productId}/reviews`)
+    }catch(err){
+        console.log(err.message)
+    }
+}
+
+export async function upvoteReview(reviewId) {
+    try {
+        const response = await axios.patch(`${API_URL}/api/reviews/${reviewId}/upvote`)
+        return response.data
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export async function downvoteReview(reviewId) {
+    try{
+        const response = await axios.patch(`${API_URL}/api/reviews/${reviewId}/downvote`)
+        return response.data
     }catch(err){
         console.log(err.message)
     }
