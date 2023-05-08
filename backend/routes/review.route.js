f9fbbf1dc4bb8d19ba12c3eef4354ddfc06e2025
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 11:19:56 +0800

    Added downvoteReview route

--- backend/routes/review.route.js
+++ backend/routes/review.route.js
@@ -2,4 +2,5 @@ const controller = require('../controllers/review.controller')
 
 module.exports = (server) => {
     server.patch("/api/reviews/:reviewId/upvote", controller.upvoteReview)
+    server.patch("/api/reviews/:reviewId/downvote", controller.downvoteReview)
 }
