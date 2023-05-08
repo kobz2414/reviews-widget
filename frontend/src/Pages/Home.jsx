6f9399e398c63fb2211563d9b95690859e4790c0
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 12:47:33 +0800

    Imported product component

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar"
+import Product from "../components/Product"
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Product />
     </>
   );
 };
