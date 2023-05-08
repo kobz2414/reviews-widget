1a2678951c9dafb5e21d819d9d6e04fc9792c463
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 16:59:59 +0800

    Removed react-query implementation

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,16 +1,11 @@
 import Navbar from "../components/Navbar"
 import Product from "../components/Product"
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 const Home = () => {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Navbar />
-        <Product />
-      </QueryClientProvider>
+      <Navbar />
+      <Product />
     </>
   );
 };
