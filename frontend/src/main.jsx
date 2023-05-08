8570fb6508050c33c18effdd5314584236c96e17
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 12:45:49 +0800

    Replaced App to Home

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from '../Pages/Home'
+import Home from './Pages/Home'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
