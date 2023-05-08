12ef589a88ea33f21e4178f6c43730753f346074
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 15:31:09 +0800

    Updated ProductReview component

---
+++ frontend/src/components/product/ProductReview.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import ProductReviewRating from "./ProductReviewRating";
+
+const ProductReview = ({ review, onVote }) => {
+  const handleVote = (id, type) => {
+    // Call the parent onVote function to handle the vote
+    onVote(id, type);
+  };
+
+  return (
+    <>
+      <div key={review.id} className="border p-4 my-4 flex items-center">
+        <div className="flex flex-col items-center">
+          <button onClick={() => handleVote(review.id, "upvote")}>
+            <img src="TriangleUp.png" alt="Up" className="h-4 flex-1 my-1" />
+          </button>
+          <span className="text-xl font-bold flex-1">{review.votes}</span>
+          <button onClick={() => handleVote(review.id, "downvote")}>
+            <img
+              src="TriangleDown.png"
+              alt="Down"
+              className="h-4 flex-1 my-1"
+            />
+          </button>
+        </div>
+        <div className="ml-4">
+          <div className="mb-3">
+            <h2 className="text-lg font-bold">
+              {review.title}
+              <span className="text-sm text-gray-500 ml-2">
+                <ProductReviewRating rating={review.rating} />
+              </span>
+            </h2>
+          </div>
+          <p className="text-gray-700 mb-4">{review.description}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductReview;
