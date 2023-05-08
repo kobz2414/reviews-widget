bbf48c80a676dd9b630137e284f99f3bf25274e2
Lorenz Kobe V. Managbanag <lorenz@ingenuity.ph>
2023-05-08 11:50:40 +0800

    Added get reviews test

--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -1,5 +1,10 @@
-import supertest from "supertest"
-const server = require('./server.js')
+const request = require('supertest')
+const server = require('../server.js')
 
 describe('/GET /reviews', () => {
+    test('should return a list of reviews', async () => {
+        const response = await request(server).get("/api/products/123123/reviews")
+        expect(response.statusCode).toBe(200)
+        expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"))
+    })
 })
